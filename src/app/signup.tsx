4ffbe72108af49e9a275ef4f20f5b79009482d80
Pedro <pedro.fialho000@gmail.com>
2026-03-13 18:02:33 -0300

    create project struture and pages: login anf signup

---
+++ src/app/signup.tsx
@@ -0,0 +1,87 @@
+import { View, Text, Image, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
+import "../../global.css";
+import { Input, InputField } from "@/components/ui/input";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Link } from "expo-router";
+
+export default function Signup() {
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View className="flex-1 bg-platinum items-center justify-start">
+
+          <View className="w-full self-center p-12">
+            <Image
+              source={require("../assets/images/logo-vdb.png")}
+              className="self-center mb-3"
+              style={{ width: 156, height: 156 }}
+            />
+
+            <Text className="font-thin text-2xl w-60 self-center text-purple-x11-600 text-center">
+              Chegar na facul nunca foi tão fácil.
+            </Text>
+          </View>
+
+          <View className="h-[1px] bg-velvet-orchid-700 w-[93%] self-center" />
+
+          <Text className="font-bold text-2xl w-60 self-center text-velvet-orchid-700 text-center mt-20">
+            Cadastre-se aqui
+          </Text>
+
+          <View className="w-[80%] self-center flex mt-14">
+
+            <Input
+              variant="rounded"
+              className="border-velvet-orchid-700 mb-4"
+            >
+              <InputField
+                placeholder="Nome completo"
+              />
+            </Input>
+
+            <Input
+              variant="rounded"
+              className="border-velvet-orchid-700 mb-4"
+            >
+              <InputField
+                placeholder="Email"
+                keyboardType="email-address"
+              />
+            </Input>
+
+            <Input
+              variant="rounded"
+              className="border-velvet-orchid-700"
+            >
+              <InputField
+                placeholder="Senha"
+                secureTextEntry
+              />
+            </Input>
+
+            <Text className="ml-4 text-velvet-orchid-700 mt-2">
+              Já possui uma conta? {" "}
+              <Link href={"/"} className="text-purple-x11-700">
+                Faça o login
+              </Link>
+            </Text>
+
+            <Button className="bg-velvet-orchid-700 w-[50%] self-center active:bg-velvet-orchid-600 rounded-full mt-6">
+              <ButtonText className="text-white">
+                Entrar
+              </ButtonText>
+            </Button>
+
+          </View>
+
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
