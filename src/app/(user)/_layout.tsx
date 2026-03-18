778ca687d74c09a219d1ba86f2ee982910966693
Pedro <pedro.fialho000@gmail.com>
2026-03-17 21:45:44 -0300

    refactor: add title in nav bar components

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -1,57 +1,61 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { FontAwesome } from "@expo/vector-icons"
-import { CarFront, Search } from "lucide-react-native";
-import Logo from "../../assets/images/logo-vdb.svg"
+import { Search } from "lucide-react-native";
 import { useEffect } from "react";
+import Logo from "../../assets/images/logo-vdb.svg";
 
-export default function UserLayout(){
-    useEffect(() => (console.log("Logo: ", Logo)), [])
-    return(
-        <Tabs
-            screenOptions={{
-            tabBarActiveTintColor: "#a571c1", // cor do ícone ativo
-            tabBarInactiveTintColor: "#f7e5ff", // cor do ícone inativo
-            tabBarStyle: {
-                backgroundColor: "#120e15",
-            }
+export default function UserLayout() {
+  useEffect(() => console.log("Logo: ", Logo), []);
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#a571c1", // cor do ícone ativo
+        tabBarInactiveTintColor: "#f7e5ff", // cor do ícone inativo
+        tabBarStyle: {
+          backgroundColor: "#120e15",
+        },
       }}
-        >
-            <Tabs.Screen
-                name="home"
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => {
-                        return <FontAwesome name="home" color={color} size={size}/>
-                    }
-                }}  
-            />
-            <Tabs.Screen
-                name="search"
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => {
-                        return <Search color={color} size={size}/>
-                    }
-                }}  
-            />
-            <Tabs.Screen
-                name="travels"
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => {
-                    return <Logo width={size} height={size} fill={color} />
-                    }
-                }}  
-            />
-            <Tabs.Screen
-                name="profile"
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => {                     
-                        return <FontAwesome name="user-circle" color={color} size={size}/>
-                    }
-                }}  
-            />
-        </Tabs>
-    )
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome name="home" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Buscar",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            return <Search color={color} size={size} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="travels"
+        options={{
+          title: "Minhas Caronas",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            return <Logo width={size} height={size} fill={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Perfil",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome name="user-circle" color={color} size={size} />;
+          },
+        }}
+      />
+    </Tabs>
+  );
 }
